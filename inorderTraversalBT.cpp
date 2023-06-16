dc01a1d685ecbf7ee035ce90954c76beb42c80a5
Harshit Pandey <harshitpandey64@gmail.com>
2023-06-16 19:19:12 +0530

    Create inorderTraversalBT.cpp

---
+++ inorderTraversalBT.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void rec(TreeNode* root,vector<int> &temp){
+    if(!root)return;
+    rec(root->left,temp);
+    temp.push_back(root->data);
+    rec(root->right,temp);
+}
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    vector<int> temp;
+    rec(root,temp);
+    return temp;
+}
