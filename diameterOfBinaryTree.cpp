380f2cc72cb2b6fe78fb35f60105b65adbcb774b
Harshit Pandey <harshitpandey64@gmail.com>
2023-06-19 15:31:12 +0530

    Create diameterOfBinaryTree.cpp

---
+++ diameterOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+int rec(TreeNode<int>* root,int &ans){
+    if(!root)return 0;
+    int left=rec(root->left,ans);
+    int right=rec(root->right,ans);
+    ans=max(ans,left+right);
+    return 1+max(left,right);
+}
+int diameterOfBinaryTree(TreeNode<int> *root){
+    int ans=0;
+	rec(root,ans);
+    return ans;
+}
